b6a9d541008c29068e97fa3bd0b7e20847bcf891
Olamide Ismail <abolarinwaismail332@gmail.com>
2026-05-24 00:14:53 +0100

    Updates

---
+++ _next/static/mgaNY30YP8tIc6aRpBE76/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/My-Portfolio//_next/:path+",
+        "destination": "/My-Portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
