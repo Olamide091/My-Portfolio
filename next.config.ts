c0d2092b8d497d44b8f1287a31f94b7dc68165a6
Olamide Ismail <abolarinwaismail332@gmail.com>
2026-05-24 07:20:06 +0100

    Fix GitHub Pages assets

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/My-Portfolio",
-  assetPrefix: "/My-Portfolio/",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/My-Portfolio" : "",
+  assetPrefix: isProd ? "/My-Portfolio/" : "",
 };
 
 export default nextConfig;
